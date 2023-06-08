bca4d13b370499ff1b6e3f781de4af580ba95ee8
keerthanamuruge <keerthuofficial2001@gmail.com>
2023-06-08 15:13:17 +0530

    home changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NewfeedComponent } from './featured/newfeed/newfeed.component';
+import { HomeComponent } from './featured/home/home.component';
 
 const routes: Routes = [
+  {path: '', redirectTo:'/home', pathMatch: 'full'},
   { path: 'newfeed', component: NewfeedComponent },
+  {path: 'home', component: HomeComponent},
 ];
 
 @NgModule({
--- src/app/featured/home/home.component.ts
+++ src/app/featured/home/home.component.ts
@@ -9,6 +9,8 @@ import { UserService } from 'src/app/core/services/user.service';
 export class HomeComponent {
   // public user_detail: ;
   public borderBottom = "border-bottom"
+  public is_global = true
+  public is_not_global = false
   // constructor(u: UserService){
   //   user_detail = this.user
   // }
@@ -38,7 +40,26 @@ export class HomeComponent {
     tag: ['heello', 'hi'],
     created_at: '2022-01-09',
     updated_at: '2022-01-09'
+  },
+  {
+    id: 23,
+    title: 'title',
+    body: 'The Journal of Living Together is a peer-reviewed academic journal that publishes a collection of articles that reflect various aspects of peace and conflict studies. The contributions from across the disciplines and grounded by relevant philosophical traditions and theoretical and methodological approaches systematically broach topics dealing with tribal, ethnic, racial, cultural, religious and sectarian conflicts, as well as alternative dispute resolution and peacebuilding processes. Through this journal it is our intention to inform, inspire, reveal and explore the intricate and complex nature of human interaction in the context of ethno-religious identity and the roles it plays in war and peace. By sharing theories, methods, practices, observations and valuable experiences we mean to open a broader, more inclusive dialogue between policymakers, academics, researchers, religious leaders, representatives of ethnic groups and indigenous peoples, as well as field practitioners around the world.',
+    user_id: 23,
+    is_global: false,
+    tag: ['heello', 'hi'],
+    created_at: '2022-01-09',
+    updated_at: '2022-01-09'
   }]
 
-  public tags = ['welcome', 'hi', 'hello']
+  public tags = ['welcome', 'hi', 'hello', 'jion', 'sdfjl']
+
+  yourfeeds(){
+    this.is_not_global = true
+    this.is_global = false
+  }
+  globalfeeds(){
+    this.is_global = true
+    this.is_not_global = false
+  }
 }
